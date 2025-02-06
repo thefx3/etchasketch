89bc0d2be9817bb20fe41b25278c2291557f0ecd
thefx3 <fx.pairault@yahoo.fr>
2025-02-06 22:38:42 +0100

    Nothing more

--- javascript.js
+++ javascript.js
@@ -1,25 +1,34 @@
 const grid = document.querySelector("div");
 
-for (let i=0; i<16; i++){
-    for (let j=0; j<16; j++){
-        const square = document.createElement("div");
-        grid.appendChild(square);
+function createGrid(x){
+  grid.innerHTML = ""; /* Clean the grid before creating */
 
-        square.addEventListener(
-            "mouseover", (event) => {
-              // highlight the mouseover target
-              event.target.style.backgroundColor = "orange";
-          
-              // reset the color after a short delay
-              setTimeout(() => {
-              event.target.style.backgroundColor = "";
-              }, 500);
-            },
-            false,
-          );
-
+    for (let i=0; i<x; i++){
+        for (let j=0; j<x; j++){
+            const square = document.createElement("div");
+            grid.appendChild(square);
+    
+            square.addEventListener(
+                "mouseover", (event) => {
+                  // highlight the mouseover target
+                  event.target.style.backgroundColor = "orange";
+              
+                  // reset the color after a short delay
+                  setTimeout(() => {
+                  event.target.style.backgroundColor = "";
+                  }, 500);
+                },
+                false,
+              );
+        }
     }
 }
 
+createGrid(16); /* Create the grid */
+ 
 
+const button = document.querySelector("button");
 
+button.addEventListener('click', function() {
+    const size = prompt("New grid size ?");
+  });
