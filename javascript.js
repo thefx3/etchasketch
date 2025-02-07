39200a515e02e6cfdb4d6a0e6aef333b08be6ae4
thefx3 <fx.pairault@yahoo.fr>
2025-02-07 23:54:59 +0100

    Add eraser and reset mode

--- javascript.js
+++ javascript.js
@@ -1,46 +1,72 @@
-const grid = document.querySelector("div");
+const grid = document.querySelector('.grid');
+grid.style.height = "800px"; 
+grid.style.width = "800px";
+let size = 16;
+let eraseMode = false;
 
-function createGrid(x){
+
+function createGrid(size){
   grid.innerHTML = ""; /* Clean the grid before creating */
-  grid.style.setProperty('--cols',x); 
 
-  for (let i=0; i<x*x; i++){
+  const gridsize = 800;
+  const squaresize = (`${gridsize/size}px`);
+
+  for (let i=0; i<size*size; i++){
 
     const square = document.createElement("div");
     square.classList.add("square");
+    square.style.width = squaresize;
+    square.style.height = squaresize;
+
     grid.appendChild(square);
 
     square.addEventListener("mouseover", (event) => {
-      // highlight the mouseover target
-      event.target.style.backgroundColor = randomColor();
-              
-      // reset the color after a short delay
-      setTimeout(() => {
-      event.target.style.backgroundColor = "";
-      }, 500);
-    },
-    false,);
+      if (eraseMode) {
+        event.target.style.backgroundColor = "";  // Efface la couleur si l'effaceur est activé
+      } else {
+        event.target.style.backgroundColor = randomColor();  // Sinon, colorie normalement
+      }
+    });
   }
 }
 
 createGrid(15); /* Create the grid */
 
+const buttons = document.querySelectorAll("button");
+const newGrid = document.querySelector('.newGrid');
+const eraser = document.querySelector('.eraser');
+const reset = document.querySelector('.reset')
 
-const button = document.querySelector("button");
-button.style.display = "block";
-button.style.margin = "20px auto";
-button.style.padding = "10px 20px";
-button.style.fontSize = "16px";
+buttons.forEach(button => {
+  button.style.display = "in-line block";
+  button.style.margin = "20px";
+  button.style.padding = "10px 20px";
+  button.style.fontSize = "16px";
+});
 
-button.addEventListener('click', function() {
-    let size = prompt("New grid size ?");
+const container = document.querySelector('.container');
+container.style.display = 'flex';
+container.style.flexDirection = 'row';
+container.style.alignItems = 'center';
+container.style.justifyContent = 'center';
+
+
+newGrid.addEventListener('click', function() {
+    size = prompt("New grid size ?");
     while(size>100 || size<0){
       size = prompt("Insert a correct value : ");
     }
     createGrid(size);
   });
 
+eraser.addEventListener('click', function() {
+  eraseMode = !eraseMode;
+  eraseMode.textContent = eraseMode ? "Drawing Mode" : "Eraser Mode"; 
+});
 
+reset.addEventListener('click', function() {
+    createGrid(size);
+  });
 
 function randomNumber(){
   let random = Math.floor(Math.random()*16);
@@ -56,3 +82,5 @@ function randomColor(){
 
   return newcolor;
 }
+
+
