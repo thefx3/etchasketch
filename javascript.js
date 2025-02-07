bda4774eca868fbafea5c8ef72a8320858e3a2a4
thefx3 <fx.pairault@yahoo.fr>
2025-02-07 14:06:08 +0100

    Grid work for any size

--- javascript.js
+++ javascript.js
@@ -2,29 +2,28 @@ const grid = document.querySelector("div");
 
 function createGrid(x){
   grid.innerHTML = ""; /* Clean the grid before creating */
+  grid.style.setProperty('--cols',x); 
 
-    for (let i=0; i<x; i++){
-        for (let j=0; j<x; j++){
-            const square = document.createElement("div");
-            grid.appendChild(square);
-    
-            square.addEventListener(
-                "mouseover", (event) => {
-                  // highlight the mouseover target
-                  event.target.style.backgroundColor = "orange";
+  for (let i=0; i<x*x; i++){
+
+    const square = document.createElement("div");
+    square.classList.add("square");
+    grid.appendChild(square);
+
+    square.addEventListener("mouseover", (event) => {
+      // highlight the mouseover target
+      event.target.style.backgroundColor = "orange";
               
-                  // reset the color after a short delay
-                  setTimeout(() => {
-                  event.target.style.backgroundColor = "";
-                  }, 500);
-                },
-                false,
-              );
-        }
-    }
+      // reset the color after a short delay
+      setTimeout(() => {
+      event.target.style.backgroundColor = "";
+      }, 500);
+    },
+    false,);
+  }
 }
 
-createGrid(16); /* Create the grid */
+createGrid(10); /* Create the grid */
  
 
 const button = document.querySelector("button");
